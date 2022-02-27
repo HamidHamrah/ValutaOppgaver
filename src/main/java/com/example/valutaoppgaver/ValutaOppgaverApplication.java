ef11add69ef1cc4ff0c984744af8d4ac92547bfc
HamidHamrah <S362052@oslomet.no>
2022-02-27 14:36:27 +0100

    Initial commit

---
+++ src/main/java/com/example/valutaoppgaver/ValutaOppgaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.valutaoppgaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValutaOppgaverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValutaOppgaverApplication.class, args);
+    }
+
+}
